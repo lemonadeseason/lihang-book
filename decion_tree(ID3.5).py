51b702075f24bfa284a2fea4ac8d9fcd0fe88e1f
lemonadeseason <31380516+lemonadeseason@users.noreply.github.com>
2019-09-19 18:35:24 +0800

    Create decion_tree(ID3.5).py

---
+++ decion_tree(ID3.5).py
@@ -0,0 +1,134 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from collections import Counter
+import math
+from math import log
+import pprint
+
+def create_data():
+    datasets = [['青年', '否', '否', '一般', '否'],
+               ['青年', '否', '否', '好', '否'],
+               ['青年', '是', '否', '好', '是'],
+               ['青年', '是', '是', '一般', '是'],
+               ['青年', '否', '否', '一般', '否'],
+               ['中年', '否', '否', '一般', '否'],
+               ['中年', '否', '否', '好', '否'],
+               ['中年', '是', '是', '好', '是'],
+               ['中年', '否', '是', '非常好', '是'],
+               ['中年', '否', '是', '非常好', '是'],
+               ['老年', '否', '是', '非常好', '是'],
+               ['老年', '否', '是', '好', '是'],
+               ['老年', '是', '否', '好', '是'],
+               ['老年', '是', '否', '非常好', '是'],
+               ['老年', '否', '否', '一般', '否'],
+               ]
+    labels = [u'年龄',u'有工作',u'有自己的房子',u'信贷情况',u'类别']
+    return datasets,labels
+
+class Node:
+    def __init__(self,root=True,label=None,feature_name=None,feature=None):
+        self.root = root
+        self.label = label    #叶节点的话，代表类别     string
+        self.feature_name = feature_name    #特征名，string
+        self.feature = feature               #feature是非叶节点中的，代表用来spilt的节点在当时数据集中在第几列    int
+        self.tree = {}    #用来保存这个特征的划分情况，是一个字典，key是这个特征的某个取值，对应value是特征为这个取值的对应树
+        self.result = {
+            'label:':self.label,  
+            'feature:':self.feature,
+            'tree:':self.tree
+        }
+        
+    def __repr__(self):
+        return '{}'.format(self.result)
+    def add_node(self,val,node):
+        self.tree[val] = node
+        
+class DTree:
+    def __init__(self,epsilon=0.1):
+        self.epsilon = epsilon
+        self._tree = {}
+    
+    #熵
+    @staticmethod
+    def calc_ent(datasets):
+        data_length = len(datasets)    #样本总数
+        label_count = {}
+        for i in range(data_length):
+            label = datasets[i][-1]
+            if label not in label_count:
+                label_count[label] = 0
+            label_count[label] += 1
+        ent = -sum(p/data_length*log(p/data_length,2)
+                   for p in label_count.values())
+        return ent
+    
+    # 经验条件熵
+    def cond_ent(self, datasets, axis=0):
+        data_length = len(datasets)
+        feature_sets = {}
+        for i in range(data_length):
+            feature = datasets[i][axis]
+            if feature not in feature_sets:
+                feature_sets[feature] = []
+            feature_sets[feature].append(datasets[i])
+        cond_ent = sum([(len(p) / data_length) * self.calc_ent(p)
+                        for p in feature_sets.values()])
+        return cond_ent
+    
+    # 信息增益
+    @staticmethod
+    def info_gain(ent, cond_ent):
+        return ent - cond_ent
+
+    def info_gain_train(self, datasets):
+        count = len(datasets[0]) - 1
+        ent = self.calc_ent(datasets)
+        best_feature = []
+        for c in range(count):
+            c_info_gain = self.info_gain(ent, self.cond_ent(datasets, axis=c))
+            best_feature.append((c, c_info_gain))
+        # 比较大小
+        best_ = max(best_feature, key=lambda x: x[-1])
+        return best_
+    
+    def train(self,train_data):
+        
+        y_train,features = train_data.iloc[:,-1],train_data.columns[:-1]
+        #1.若样本都属于同一个类别，则结束，并且用这个类别作为节点的类标记
+        if len(y_train.value_counts())==1:
+            return Node(root=True,label=y_train.iloc[0])    #根？
+        
+        #2.如果没有用来分类的特征(之前用来分类的是仅剩的最后一个特征了，现在只能用类别中占大多数的作为这个节点的类别)
+        if len(features)==0:
+            return Node(root=True,label=y_train.value_counts().sort_values(ascending=False).index[0])
+        
+        #3.计算各特征对数据集的信息增益
+        #需要np.array吗？
+        max_feature, max_info_gain = self.info_gain_train(np.array(train_data))
+        max_feature_name = features[max_feature]
+        
+        #4.如果最大信息增益小于阈值，则不再继续分类
+        if max_info_gain<self.epsilon:
+            return Node(root=True,label=y_train.value_counts().sort_values(ascending=False).index[0])
+        
+        #5.否则，继续用带来最大信息增益的特征对样本分类
+        node_tree = Node(root=False,feature_name=max_feature_name,feature=max_feature)
+        
+        feature_list = train_data[max_feature_name].value_counts().index #这一特征在样本集上所有可能的取值
+        for f in feature_list:
+            #这里的pandas的index语法不太明白
+            sub_train_df = train_data.loc[train_data[max_feature_name]==f].drop([max_feature_name],axis=1)
+            sub_tree = self.train(sub_train_df)
+            node_tree.add_node(f,sub_tree)      
+            
+        return node_tree
+    
+    def fit(self,train_data):
+        self._tree = self.train(train_data)
+        return self._tree
+    
+datasets, labels = create_data()
+data_df = pd.DataFrame(datasets, columns=labels)
+dt = DTree()
+tree = dt.fit(data_df)
+print tree
